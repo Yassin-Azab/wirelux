1169db7136d552c72e8d00a920230368e509828e
Yassin-Azab <ya.azab@yahoo.com>
2026-06-10 00:52:41 +0300

    begin packet sniffing, add ctrl c support

--- main.cpp
+++ main.cpp
@@ -25,6 +25,15 @@
 #include <chrono> // Required for modern time handling
 // ── C++ standard library ──
 
+void packethandler(u_char *user, const struct pcap_pkthdr *h, u_char *bytes){
+
+}
+
+pcap_t* g_handle = nullptr;  
+
+void on_ctrl_c(int) {
+    if (g_handle) {pcap_breakloop(g_handle);}
+}
 int main{
 
 pcap_if_t* AllInterfaces = nullptr;
@@ -35,22 +44,30 @@ if (pcap_findalldevs(pcap_if_t **AllInterfaces, char *InterfaceErrorrs) == -1) {
 }
 int i=1;
 pcap_if_t* chosenInt=nullptr;
-for (pcap_if_t* CurrInt = AllInterfaces; CurrInt=(*CurrInt).next) {
-    if(!(CurrInt->flags & PCAP_IF_LOOPBACK)&& CurrInt ->flags & PCAP_IF_RUNNING && (dev->flags & PCAP_IF_CONNECTION_STATUS) == PCAP_IF_CONNECTION_STATUS_CONNECTED) ;
-        std::string line =std::format("{}. Interface Name: {}", i, (*CurrInt).name);
+for (pcap_if_t* CurrInt = AllInterfaces;CurrInt!=nullptr ;CurrInt=(*CurrInt).next) {
+    if(!(CurrInt->flags & PCAP_IF_LOOPBACK)&& CurrInt ->flags & PCAP_IF_RUNNING && (dev->flags & PCAP_IF_CONNECTION_STATUS) == PCAP_IF_CONNECTION_STATUS_CONNECTED) && std::string(CurrInt->name) != "any" ;
+        {std::string line =std::format("{}. Interface Name: {}", i, (*CurrInt).name);
         std::cout << line << std::endl;
-        i++;
+        ++i;
         if (chosenInt==nullptr && (CurrInt->name).std::string::contains(wl))
             chosenInt=CurrInt;
         else if (chosenInt==nullptr && CurrInt->next==nullptr)
             chosenInt=CurrInt;
+        }
 }
 
 std::chrono::auto startTime = std::chrono::steady_clock::now(); + seconds(60);
-while (std::chrono::steady_clock::now() < startTime) {
-  
+g_handle =pcap_open_live(chosenInt->name, 65536, 1, 1000, InterfaceErrorrs);
+if (g_handle == nullptr) {
+    std::cerr << "Error opening device: " << InterfaceErrorrs << std::endl;
+    return 2;
 }
-
+signal(SIGINT, on_ctrl_c);
+int pcap_loop(pcap_t* g_handle, -1, &packethandler, u_char *user);
+void pcap_breakloop(pcap_t *handle);
 void pcap_freealldevs(pcap_if_t *AllInterfaces);
+pcap_close(g_handle);
 AllInterfaces=nullptr;
+g_handle = nullptr;
+return 0;
 }
