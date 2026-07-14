440dceeb442e82feecf1b15e83fc79d59148c9a5
Yassin-Azab <61655278+Yassin-Azab@users.noreply.github.com>
2026-07-15 00:38:42 +0300

    Define RingBuf+WriteEvent

--- wirelux/wirelux-ebpf/src/main.rs
+++ wirelux/wirelux-ebpf/src/main.rs
@@ -1,9 +1,25 @@
 #![no_std]
 #![no_main]
 
-use aya_ebpf::{macros::kprobe, programs::ProbeContext};
+use aya_ebpf::{
+    macros::{kprobe,kretprobe, map},
+    programs::{ProbeContext, RetProbeContext}
+    maps::RingBuf,
+    helpers::{bpf_get_current_comm,bpf_get_current_pid_tgid,bpf_ktime_get_real_ns}
+};
 use aya_log_ebpf::info;
+use wirelux_common::AppBytes;
 
+const Ring_buf_Size: u32= 1024*1024*4
+    
+    #[map]
+    static mut Events: RingBuf=RingBuf::with_size_bytes(Ring_Buf_Size,0);
+
+
+unsafe fn write_event(pid::u32, comm:[u8;16], bytes::u64, direction:u8, protocol:: u8) -> Result<(),i64>{
+
+}
+                      
 #[kprobe]
 pub fn wirelux(ctx: ProbeContext) -> u32 {
     match try_wirelux(ctx) {
