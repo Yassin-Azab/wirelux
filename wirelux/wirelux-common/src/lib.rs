2fd98e0e695c6d31181fcf2170502bf4b5c05805
Yassin-Azab <ya.azab@yahoo.com>
2026-07-15 19:28:05 +0300

    write event completed+probe method definition defined

--- wirelux/wirelux-common/src/lib.rs
+++ wirelux/wirelux-common/src/lib.rs
@@ -1,13 +1,13 @@
 #![no_std]
 
 #[repr(C)]
-#[derive(Clone,Copy,Debug,default)]
+#[derive(Clone,Copy,Debug, Default)]
 pub struct AppBytes{
     pub timestamp_ns: u64,
-    pub comms: [u8:16],
+    pub comms: [u8;16],
     pub size: u64,
     pub protocol: u8,
     pub direction: u8,
     pub pid: u32,
-    pub _pad: [u8,2]
+    pub _pad: [u8;2]
 }
--- wirelux/wirelux-ebpf/src/main.rs
+++ wirelux/wirelux-ebpf/src/main.rs
@@ -1,23 +1,41 @@
 #![no_std]
 #![no_main]
 
+use core::f32::consts::E;
+
 use aya_ebpf::{
     macros::{kprobe,kretprobe, map},
-    programs::{ProbeContext, RetProbeContext}
+    programs::{ProbeContext, RetProbeContext},
     maps::RingBuf,
-    helpers::{bpf_get_current_comm,bpf_get_current_pid_tgid,bpf_ktime_get_real_ns}
+    helpers::{bpf_get_current_comm,bpf_get_current_pid_tgid,bpf_ktime_get_ns}
 };
 use aya_log_ebpf::info;
 use wirelux_common::AppBytes;
 
-const Ring_buf_Size: u32= 1024*1024*4
+const Ring_buf_Size: u32= 1024*1024*4;
     
     #[map]
-    static mut Events: RingBuf=RingBuf::with_size_bytes(Ring_Buf_Size,0);
+    static EVENTS: RingBuf=RingBuf::with_byte_size(Ring_buf_Size, 0);
 
 
-unsafe fn write_event(pid::u32, comm:[u8;16], bytes::u64, direction:u8, protocol:: u8) -> Result<(),i64>{
-
+unsafe fn write_event(pid:u32, comm:[u8;16], bytes:u64, direction:u8, protocol: u8) -> Result<(),i64>{
+let timestamp_now=unsafe { bpf_ktime_get_ns() };
+let mut entry= match EVENTS.reserve::<AppBytes>(0){
+Some(e)=>e,
+None=>return Err((-1))
+};
+let ptr=entry.as_mut_ptr();
+unsafe{
+    (*ptr).timestamp_ns=timestamp_now;
+    (*ptr).comms=comm;
+    (*ptr).size=bytes;
+    (*ptr).protocol=protocol;
+    (*ptr).direction=direction;
+    (*ptr).pid=pid;
+    (*ptr)._pad = [0;2];
+};
+entry.submit(0);
+Ok(())
 }
                       
 #[kprobe]
@@ -32,6 +50,53 @@ fn try_wirelux(ctx: ProbeContext) -> Result<u32, u32> {
     info!(&ctx, "kprobe called");
     Ok(0)
 }
+
+#[kprobe]
+pub fn kprobe_tcp_send_msg(ctx: ProbeContext) -> u32{
+    match try_tcp_sendmsg(&ctx){
+    }
+}
+
+unsafe fn try_tcp_sendmsg(ctx: &ProbeContext) -> Result<(), i64>{
+
+}
+
+
+#[kprobe]
+pub fn kprobe_udp_send_msg(ctx: ProbeContext) -> u32{
+    match try_udp_sendmsg(&ctx){
+    }
+}
+
+unsafe fn try_udp_sendmsg(ctx: &ProbeContext) -> Result<(), i64>{
+
+}
+
+#[kretprobe]
+pub fn kretprobe_tcp_recv_msg(ctx: ProbeContext) -> u32{
+    match try_tcp_recvmsg(&ctx){
+
+    }
+}
+
+unsafe fn try_tcp_recvmsg(ctx: &ProbeContext) -> Result<(), i64>{
+
+}
+
+
+#[kretprobe]
+pub fn kretprobe_tcp_recv_msg(ctx: ProbeContext) -> u32{
+    match try_tcp_recvmsg(&ctx){
+
+    }
+}
+
+unsafe fn try_tcp_recvmsg(ctx: &ProbeContext) -> Result<(), i64>{
+    
+}
+
+
+
 
 #[cfg(not(test))]
 #[panic_handler]
