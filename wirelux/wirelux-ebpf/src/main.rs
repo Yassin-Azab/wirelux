053f673f601666c3f9df5173a81a03683bfa5cbb
Yassin-Azab <ya.azab@yahoo.com>
2026-07-15 20:24:44 +0300

    eBPF completed

--- wirelux/wirelux-ebpf/src/main.rs
+++ wirelux/wirelux-ebpf/src/main.rs
@@ -1,21 +1,18 @@
 #![no_std]
 #![no_main]
 
-use core::f32::consts::E;
-
 use aya_ebpf::{
     macros::{kprobe,kretprobe, map},
     programs::{ProbeContext, RetProbeContext},
     maps::RingBuf,
     helpers::{bpf_get_current_comm,bpf_get_current_pid_tgid,bpf_ktime_get_ns}
 };
-use aya_log_ebpf::info;
 use wirelux_common::AppBytes;
 
-const Ring_buf_Size: u32= 1024*1024*4;
+const RING_BUF_SIZE: u32= 1024*1024*4;
     
     #[map]
-    static EVENTS: RingBuf=RingBuf::with_byte_size(Ring_buf_Size, 0);
+    static EVENTS: RingBuf=RingBuf::with_byte_size(RING_BUF_SIZE, 0);
 
 
 unsafe fn write_event(pid:u32, comm:[u8;16], bytes:u64, direction:u8, protocol: u8) -> Result<(),i64>{
@@ -37,73 +34,91 @@ unsafe{
 entry.submit(0);
 Ok(())
 }
-                      
-#[kprobe]
-pub fn wirelux(ctx: ProbeContext) -> u32 {
-    match try_wirelux(ctx) {
-        Ok(ret) => ret,
-        Err(ret) => ret,
-    }
-}
-
-fn try_wirelux(ctx: ProbeContext) -> Result<u32, u32> {
-    info!(&ctx, "kprobe called");
-    Ok(0)
-}
 
 #[kprobe]
 pub fn kprobe_tcp_send_msg(ctx: ProbeContext) -> u32{
-    match try_tcp_sendmsg(&ctx){
+    match unsafe {try_tcp_sendmsg(&ctx)}{
+        Ok(_) | Err(_) => 0,
+
     }
 }
 
 unsafe fn try_tcp_sendmsg(ctx: &ProbeContext) -> Result<(), i64>{
+let arg: usize=ctx.arg(2).ok_or(-1i64)?;
+if arg ==0 { return Err((-1i64)); }
+
+let total_id:u64=bpf_get_current_pid_tgid();
+let pid: u32 = (total_id >> 32) as u32;
+
+let comm:[u8;16]=bpf_get_current_comm().map_err(|e|e as i64)?;
+write_event(pid, comm, arg as u64, 1, 6)
 
 }
 
 
 #[kprobe]
 pub fn kprobe_udp_send_msg(ctx: ProbeContext) -> u32{
-    match try_udp_sendmsg(&ctx){
+    match unsafe {try_udp_sendmsg(&ctx)}{
+        Ok(_) | Err(_) => 0,
     }
 }
 
 unsafe fn try_udp_sendmsg(ctx: &ProbeContext) -> Result<(), i64>{
+let arg: usize=ctx.arg(2).ok_or(-1i64)?;
+if arg ==0 { return Err((-1i64)); }
+
+let total_id:u64=bpf_get_current_pid_tgid();
+let pid: u32 = (total_id >> 32) as u32;
+
+let comm:[u8;16]=bpf_get_current_comm().map_err(|e|e as i64)?;
+write_event(pid, comm, arg as u64, 1, 17)
 
 }
 
 #[kretprobe]
-pub fn kretprobe_tcp_recv_msg(ctx: ProbeContext) -> u32{
-    match try_tcp_recvmsg(&ctx){
-
+pub fn kretprobe_tcp_recv_msg(ctx: RetProbeContext) -> u32{
+    match unsafe{ try_tcp_recvmsg(&ctx)}{
+    Ok(_)| Err(_)=> 0,
     }
 }
 
-unsafe fn try_tcp_recvmsg(ctx: &ProbeContext) -> Result<(), i64>{
+unsafe fn try_tcp_recvmsg(ctx: &RetProbeContext) ->  Result<(), i64>{
+let arg:i64 =ctx.ret() as i64;
+if arg < 0 {return Err((-1i64))}
+let total_id:u64=bpf_get_current_pid_tgid();
+let pid: u32 = (total_id >> 32) as u32;
+
+let comm:[u8;16]=bpf_get_current_comm().map_err(|e|e as i64)?;
+write_event(pid, comm, arg as u64, 0, 17)
 
 }
 
 
 #[kretprobe]
-pub fn kretprobe_tcp_recv_msg(ctx: ProbeContext) -> u32{
-    match try_tcp_recvmsg(&ctx){
-
+pub fn kretprobe_udp_recv_msg(ctx: RetProbeContext) -> u32{
+    match unsafe{try_udp_recvmsg(&ctx)}{
+        Ok(_)| Err(_)=> 0,
     }
 }
 
-unsafe fn try_tcp_recvmsg(ctx: &ProbeContext) -> Result<(), i64>{
-    
+unsafe fn try_udp_recvmsg(ctx: &RetProbeContext) -> Result<(), i64>{
+let arg:i64 =ctx.ret() as i64;
+if arg < 0 {return Err((-1i64))}
+let total_id:u64=bpf_get_current_pid_tgid();
+let pid: u32 = (total_id >> 32) as u32;
+
+let comm:[u8;16]=bpf_get_current_comm().map_err(|e|e as i64)?;
+write_event(pid, comm, arg as u64, 0, 7)   
 }
 
 
-
-
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
-}
 
 #[unsafe(link_section = "license")]
 #[unsafe(no_mangle)]
 static LICENSE: [u8; 13] = *b"Dual MIT/GPL\0";
+
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    unsafe { core::hint::unreachable_unchecked() }
+}
