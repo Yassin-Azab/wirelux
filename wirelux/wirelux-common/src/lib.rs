0fd9ded47bfc2d0d41e8bac5f9732d8a71c61ebd
Yassin-Azab <ya.azab@yahoo.com>
2026-07-17 10:31:05 +0300

    new AppBytes

--- wirelux/wirelux-common/src/lib.rs
+++ wirelux/wirelux-common/src/lib.rs
@@ -4,10 +4,15 @@
 #[derive(Clone,Copy,Debug, Default)]
 pub struct AppBytes{
     pub timestamp_ns: u64,
-    pub comms: [u8;16],
     pub size: u64,
-    pub protocol: u8,
-    pub direction: u8,
+    pub remote_addr: u32,
     pub pid: u32,
-    pub _pad: [u8;2]
+    pub local_port: u16,
+    pub direction: u8,
+    pub protocol: u8,
+    pub comm: [u8;16],
+    pub _pad: [u8; 4],
 }
+
+#[cfg(feature = "user")]
+unsafe impl aya::Pod for AppBytes{}
