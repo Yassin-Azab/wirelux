051f4778b27aaed71447d5af10b5cc2d492fbc22
Yassin-Azab <ya.azab@yahoo.com>
2026-07-17 17:36:50 +0300

    Xtask in the wrong place

--- xtask/target/debug/build/anyhow-f7c0c8d5c66a1b47/build_script_build-f7c0c8d5c66a1b47.d
+++
@@ -1,5 +0,0 @@
-/home/hammer/Documents/Github/Wirelux/xtask/target/debug/build/anyhow-f7c0c8d5c66a1b47/build_script_build-f7c0c8d5c66a1b47.d: /home/hammer/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anyhow-1.0.103/build.rs
-
-/home/hammer/Documents/Github/Wirelux/xtask/target/debug/build/anyhow-f7c0c8d5c66a1b47/build_script_build-f7c0c8d5c66a1b47: /home/hammer/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anyhow-1.0.103/build.rs
-
-/home/hammer/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anyhow-1.0.103/build.rs:
--- xtask/target/debug/deps/xtask-f93b8d90e95a295a.d
+++
@@ -1,7 +0,0 @@
-/home/hammer/Documents/Github/Wirelux/xtask/target/debug/deps/xtask-f93b8d90e95a295a.d: src/main.rs
-
-/home/hammer/Documents/Github/Wirelux/xtask/target/debug/deps/libxtask-f93b8d90e95a295a.rmeta: src/main.rs
-
-src/main.rs:
-
-# env-dep:CARGO_MANIFEST_DIR=/home/hammer/Documents/Github/Wirelux/xtask
